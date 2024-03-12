4563dddee81a6e51be673e72be20616a73d9e6b3
kimminsum <tommy1005a@gmail.com>
2024-03-13 00:47:38 +0900

    build settings

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='news-scraper',
     version='0.0.1',
-    url='https://github.com/morganbarber/python-news-scraper',
+    url='https://github.com/kimminsum/news-scraper.git',
     author='kar7mp5',
     author_email='tommy1005a@gmail.com',
     description='A python package to scrape news.',
     packages=find_packages(),    
-    install_requires=['requests', 'beautifulsoup4', 'urllib', 'selenium'],
+    install_requires=['requests', 'beautifulsoup4', 'selenium', 'pandas'],
 )
