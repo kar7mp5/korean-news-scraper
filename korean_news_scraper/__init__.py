42be79b67ed30bee3b04ddd362b822aa7df61098
kimminsum <tommy1005a@gmail.com>
2024-03-13 23:18:12 +0900

    fix bug

--- korean_news_scraper/__init__.py
+++ korean_news_scraper/__init__.py
@@ -1,6 +1,6 @@
 from os.path import dirname
 from sys import path
 
-path.insert( 0 , dirname( __file__ ) )
+path.insert(0, dirname(__file__))
 from save_data_csv import save_data_csv
-from get_article_contents import get_article_contents
+# from get_article_contents import get_article_contents
